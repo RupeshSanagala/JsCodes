342530458dfa9a1822e69b573c6c314398e29073
RUPESH KUMAR REDDY SANAGALA <116737181+RupeshSanagala@users.noreply.github.com>
2024-11-29 18:32:54 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,40 @@
+var name = "rupesh";
+console.log(name);
+console.log(typeof(name));
+
+
+let city = null;
+console.log(typeof(city));
+console.log(city);
+
+const num = 3.14;
+console.log(num);
+
+//array
+let arr = [1,3,45,66,78];
+console.log(arr);
+console.log(typeof(arr));
+
+let emp ={
+    id:101,
+    name:"rupesh",
+    role:"PAT"
+}
+
+console.log(emp);
+console.log(typeof(emp));
+
+console.log(Math.ceil(2,1));
+
+let num1=5
+let num2='5'
+console.log(num1==num2)
+
+let num3=5
+let num4=5
+console.log(num3===num4)
+
+// document.getElementById("d1").innerHTML="rupesh";
+// document.getElementById("d2").innerHTML="rupesh";
+// document.getElementById("d3").innerHTML="rupesh";
+// document.getElementById("d4").innerHTML="rupesh";
---
+++ calculatortask.js
@@ -0,0 +1,15 @@
+let ans1=function add(num1,num2){
+    return num1+num2;
+}
+let ans2= function sub(num1,num2){
+    return num1-num2;
+}
+let ans3= function div(num1,num2){
+    return num1/num2;
+}
+let ans4=function mul(num1,num2){
+    return num1*num2;
+}
+
+document.getElementById("res").innerHTML=ans1
+document.write(ans1);
